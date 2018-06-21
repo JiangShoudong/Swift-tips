129f879ccb30db7b509fda11af0128e45b5d6136
jiangshoudong <jiangshoudong@wecash.net>
2018-06-21 20:09:52 +0800

    学习associatedtype

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -183,8 +183,47 @@ func appendQuery(url: String,
  **/
 
 /*
- 15.associdatedtype：重要未看~~~
+ 15.associdatedtype：
  **/
+// 在swift协议中可以定义属性和方法，并要求满足这个协议的类型实现它。
+protocol Food {
+    
+}
 
+protocol Animal {
+    // 使用 associatedtype 我们就可以在Animal协议中添加一个限定，让Tiger来指定食物的具体类型。associatedtype 声明中可以使用冒号来指定类型满足某个协议，另外，在Tiger中只要实现了正确类型的eat，F的类型就可以被推断出来，所以我们也不需要显式地写明F。
+    
+    // 不过再添加associatedtype之后，Animal协议就不能被当做独立的类型使用了。当我们当做独立类型使用时，编译器会报错：protocol 'Animal' can only be used as a generic constraint because it has Self or associated type requirements。这是因为swift需要在编译时确定所有类型，这里因为Animal包含一个不确定类型，所以随着Animal本身类型的变化，其中的F无法确定。在一个协议加入了像是associatedtype或者Self的约束后，它将只能被用作泛型约束，而不能作为独立类型的占位使用，也失去了动态派发的特性。也就是说这种情况下我们需要将函数改写为泛型。
+    associatedtype F: Food
+    func eat(_ food: F)
+}
 
+func isDangerous<T: Animal>(animal: T) -> Bool {
+    return animal is Tiger
+}
+
+struct Meat: Food { }
+struct Grass: Food { }
+
+struct Tiger: Animal {
+    func eat(_ food: Meat) {
+        // 因为老虎不吃素，所以我们需要在这个方法中进行一些转换，而很多时候这些转换时没有意义的，而且将责任推给了运行时。有没有更好的方式在编译时就确保老虎不吃素呢？我们可能想到的方案之一就是把协议中的函数实现里参数类型改为Meat，但是很遗憾，Meat和Food并不等价。其实：在协议中我们除了定义属性和方法外，我们还能定义类型占位符，让协议的类型来指定具体的类型。使用associatedtype我们就可以在Animal协议中添加一个限定，让Tiger来指定具体食物的类型。
+//        if let meat = food as? Meat {
+//            print("eat\(meat)")
+//        } else {
+//            fatalError("Tiger can only eat meat!")
+//        }
+        print("eat\(food)")
+    }
+}
+struct Sheep: Animal {
+    func eat(_ food: Grass) {
+        print("eat\(food)")
+    }
+}
+let meat = Meat()
+Tiger().eat(meat)
+
+let grass = Grass()
+Sheep().eat(grass)
 
